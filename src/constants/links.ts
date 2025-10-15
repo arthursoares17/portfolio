3848e61e69c181fc267dce828bacad2dc456ad43
Arthur Soares <arthursoares0217@gmail.com>
2025-10-14 21:00:40 -0300

    chore: add links constants

---
+++ src/constants/links.ts
@@ -0,0 +1,7 @@
+export const LINKS = {
+  WHATSAPP_URL: 'https://wa.me/5524999023949',
+  DRIVE_FILE_URL: 'https://drive.google.com/file/d/1tgFq34beFiHyH6XGQObuavQ_ty5VCa3p/view?usp=sharing',
+  LINKEDIN_URL: 'https://www.linkedin.com/in/arthursoares0217',
+  GITHUB_URL: 'https://github.com/arthursoares17',
+  GMAIL_URL: 'mailto:arthursoares0217@gmail.com'
+}
